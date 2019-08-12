ba775b8ceb2119eaa9ad11f9dc4b340761ce44e2
Sung Dongho <fencer0565@gmail.com>
2019-08-13 01:52:04 +0900

    Updates

---
+++ precache-manifest.2fa1a734c10daeff45bdc82a9d05f706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e7b5f7b7659767a9c473bff9e96799",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "ce89a7c167efb3fe6dd2",
+    "url": "/movie_app_2019/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "0412129e6fe003322cd1",
+    "url": "/movie_app_2019/static/js/2.af5468dd.chunk.js"
+  },
+  {
+    "revision": "ce89a7c167efb3fe6dd2",
+    "url": "/movie_app_2019/static/js/main.2c2fbe55.chunk.js"
+  },
+  {
+    "revision": "ba3076a0c8d176265049",
+    "url": "/movie_app_2019/static/js/runtime~main.1d21743e.js"
+  }
+]);
